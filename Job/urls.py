8b826180c9967ac69a79c51f9044b2569c2a7cd5
unknown <ashvinthadevoos2001@gmail.com>
2023-04-21 14:32:42 +0530

    compnay web

--- Job/urls.py
+++ Job/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from Job import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("signup/",views.SignUpView.as_view(),name="signup"),
     path("",views.SignInView.as_view(),name="signin"),
     path("home/",views.IndexView.as_view(),name="home"),
+    path("company/",views.EmployerIndexView.as_view(),name="company-home"),
     path("logout/",views.signout_view,name="signout"),
-]
+    path("company/" ,views.CompanyProfileCreateView.as_view(),name="company"),
+    path("companydetails/",views.CompanyProfileDetailView.as_view(),name="companydetails"),
+    path("companyupdate/",views.CompanyProfileUpdateView.as_view(),name="companyupdate"),
+    
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- Job/views.py
+++ Job/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,redirect
-from api.models import User
-from Job.forms import RegistrationForm,LoginForm
-from django.views.generic import View,CreateView,FormView,TemplateView
+from api.models import User,CandidateProfile,CompanyProfile,Job,Application
+from Job.forms import RegistrationForm,LoginForm,CompanyProfileForm,JobForm
+from django.views.generic import View,CreateView,FormView,TemplateView,UpdateView,ListView
 from django.urls import reverse_lazy
 from django.contrib.auth import authenticate,login,logout
+
 
 class SignUpView(CreateView):
 
@@ -29,7 +30,10 @@ class SignInView(FormView):
             usr=authenticate(request,username=usrn,password=passw)
             if usr:
                 login(request,usr)
-                return redirect("home")
+                if request.user.role == 'employer':
+                    return redirect("company-home")
+                elif request.user.role == 'candidate':
+                    return redirect("home")
             else:
                 return render(request,"login.html",{"form":form})
             
@@ -40,3 +44,59 @@ def signout_view(request,*args,**kwargs):
 
 class IndexView(TemplateView):
     template_name="index.html"
+
+class EmployerIndexView(TemplateView):
+    template_name="company.html"
+
+class CompanyProfileCreateView(CreateView):
+    model = CompanyProfile
+    form_class = CompanyProfileForm
+    template_name ='companyprofile.html'
+    success_url = reverse_lazy("companydetails")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+
+class CompanyProfileDetailView(TemplateView):
+    template_name='companyprofile_detail.html'
+
+
+
+class CompanyProfileUpdateView(UpdateView):
+    model=CompanyProfile
+    form_class=CompanyProfileForm
+    template_name="companyprofile_edit.html"
+    success_url=reverse_lazy("company")
+    pk_url_kwarg="id"
+
+class JobAddView(CreateView):
+    model=Job
+    form_class=JobForm
+    template_name='job-add.html'
+    success_url=reverse_lazy('employer-home')
+
+    def form_valid(self, form):
+        company=CompanyProfile.objects.get(user=self.request.user)
+        form.instance.company=company
+        return super().form_valid(form)
+
+class JobEditView(UpdateView):
+    model=Job
+    form_class=JobForm
+    template_name='job-add.html'
+    success_url=reverse_lazy('employer-home')
+
+class JobListView(ListView):
+    model=Job
+    template_name='job-list.html'
+    context_object_name='jobs'
+
+class JobDeleteView(View):
+    def get(self,request,*args,**kwargs):
+        id=kwargs.get('pk')
+        Job.objects.get(id=id).delete()
+        return redirect('job-list')
+
